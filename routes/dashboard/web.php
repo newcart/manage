f38d5e9a05cea93c5de638fca06a85efc656a3b1
Onur SEDEF <onrsedef@gmail.com>
2022-09-19 13:17:49 +0300

    routes fixed 2

--- routes/dashboard/web.php
+++ routes/dashboard/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::group(['middleware' => ['auth'], 'prefix' => 'dashboard'], function () {
     Route::get('/', [HomeController::class, 'dashboard'])->name('panel');
     Route::name('panel.')->group(function() {
-        include 'users\web.php';
+        include 'users/web.php';
         include 'products.php';
     });
 });
